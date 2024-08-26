9fb02b113a00fd7fd80083ac43f44ec43fbd103e
nguyenduchung <nguyenduchung22011996@gmail.com>
2024-08-26 17:05:23 +0700

    Add project files.

---
+++ Identity/Areas/Identity/Data/DBContextSample.cs
@@ -0,0 +1,33 @@
+﻿
+using Identity.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Identity.Areas.Identity.Data;
+
+public class DBContextSample : IdentityDbContext<SampleUser>,DbContext<Student>
+{
+    public DBContextSample(DbContextOptions<DBContextSample> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
+    }
+}
+
+internal class ApplicationUserEntityConfiguration : IEntityTypeConfiguration<SampleUser>
+{
+    public void Configure(EntityTypeBuilder<SampleUser> builder)
+    {
+        builder.Property(x => x.firtName).HasMaxLength(100);
+        builder.Property(x => x.lastName).HasMaxLength(100);
+    }
+}
---
+++ Identity/Lythuyet.cs
@@ -0,0 +1,21 @@
+﻿namespace Identity
+{
+    public class Lythuyet
+    {
+
+        /*
+         * ASP.NET Core Identity là một hệ thống thành viên bổ sung khả năng đăng ký và đăng nhập của người dùng vào Giao diện người dùng web ASP.NET Core.
+         * Hệ thống thành viên xử lý cả mối quan tâm về xác thực và ủy quyền. Xác thực liên quan đến việc bạn là ai. 
+         * Ủy quyền liên quan đến những gì bạn được phép làm. Do đó, xác thực là điều kiện tiên quyết để ủy quyền.
+
+            Là một giải pháp thay thế cho việc tạo tài khoản cục bộ, Identity hỗ trợ các nhà cung cấp đăng nhập bên ngoài như Facebook và Twitter. 
+            Phiên người dùng được bảo toàn bằng cách sử dụng xác thực dựa trên cookie. Theo mặc định, cookie được tạo khi đăng nhập và bị hủy khi đăng xuất.
+
+            Dữ liệu thành viên được lưu trữ bằng kho dữ liệu và công nghệ truy cập dữ liệu do bạn lựa chọn. 
+            Công nghệ truy cập dữ liệu mặc định là Object-Relational Mapper (O/RM) có tên là Entity Framework (EF) Core. Kho dữ liệu mặc định là SQL Server.
+
+            EF Core tóm tắt những phức tạp của việc tương tác với cơ sở dữ liệu cơ bản. Do đó, EF Core thường cho phép sử dụng bất kỳ nhà cung cấp cơ sở dữ liệu nào với Identity. 
+            Nhà cung cấp cơ sở dữ liệu có sẵn cho SQL Server, SQLite và một số kho dữ liệu khác. Identity cũng cung cấp tính linh hoạt để sử dụng công nghệ truy cập dữ liệu theo lựa chọn của bạn. Dapper là một giải pháp thay thế phổ biến.
+         */
+    }
+}
